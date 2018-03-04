12c87cd9b796bdd504ce341f6b144d5001be971c
pranathi <pvenigandla@gmail.com>
2018-03-04 01:30:20 -0500

    Notifications work

--- app/src/main/java/hackfsu/apx/notification/NotificationActivity.java
+++ app/src/main/java/hackfsu/apx/notification/NotificationActivity.java
@@ -1,8 +1,23 @@
 package hackfsu.apx.notification;
 
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
 /**
  * Created by pranathi on 3/3/18.
  */
 
-public class NotificationActivity {
+public class NotificationActivity extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+       // Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
 }
